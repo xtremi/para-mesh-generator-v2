809d315112b2b8b7fc64fd6fbe298e9edc95804a
remi <remilanza@gmail.com>
2022-02-22 21:20:35 +0100

    forgot to commit - unsure what changed

--- para-mesh-generator-v2/para-mesh-generator-v2.cpp
+++ para-mesh-generator-v2/para-mesh-generator-v2.cpp
@@ -1,5 +1,12 @@
+#include "paraMeshGeneratorDll.h"
+
 
 int main(int argc, char* argv[]) {
+
+	int32_t res = test1(1, 2);
+	if (res != 3) return 1;
+	res = test2(1, 2);
+	if (res != -1) return 1;
 
 	return 0;
 }
