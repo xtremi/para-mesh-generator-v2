7c401b71d5a3637f3d99840ae57e274f642a9275
Remi Lanza <remi.lanza@jotne.com>
2022-03-10 13:10:21 +0100

    ongoing PathMesher fix

--- paraMeshGeneratorLib/PathMesher.cpp
+++ paraMeshGeneratorLib/PathMesher.cpp
@@ -14,19 +14,27 @@ void PathMesher::writeNodes(
 	std::vector<int> nnodesVec;
 	double dpath = 0.;
 	if (requiredNodeLocations.size() > 0) {
-		
 
-		int node_i = 0;
+		int	   node_i = 0;
 		double curPathLoc = 0.0;
 		double remainder = 0.0;
-		for (double nodeLoc : requiredNodeLocations) {
+
+		for(int j = 0; j < requiredNodeLocations.size(); j++){
+		//for (double nodeLoc : requiredNodeLocations) {
+			double nodeLoc = requiredNodeLocations[j];
 
 			dpath = (1. - curPathLoc) / (double)(nnodes - node_i - 1);	//normalized approx. path step
 
-			double pathLength = nodeLoc - curPathLoc;
-			int nElseg = (int)(pathLength / dpath);						//number of element that fit on segment with dpath size
-			if (nElseg == 0) nElseg = 1;
-			double actualDpath = pathLength / (double)nElseg;			//actual normalized dpath for nElSeg on segment
+			double segmentLength = nodeLoc - curPathLoc;
+			int nElseg = (int)(segmentLength / dpath);						//number of element that fit on segment with dpath size
+			if (nElseg == 0) nElseg = 1;									//cannot have 0 elements
+						
+			if (j == (requiredNodeLocations.size() - 1)) {
+				if ((node_i + nElseg + 1) < nnodes) {
+					nElseg++;
+				}
+			}
+			double actualDpath = segmentLength / (double)nElseg;			//actual normalized dpath with nElSeg on segment
 
 			for (int i = 0; i < nElseg; i++) {
 				if (!skip(node_i, nnodes, nskip)) {
